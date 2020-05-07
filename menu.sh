2088285501982fb07377eb8d21c0668ef71ffd7e
Mr-Phil1 <36644192+Mr-Phil1@users.noreply.github.com>
2020-05-07 20:17:59 +0200

    add some comments

--- menu.sh
+++ menu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+######here  all the variables#######################################################################################
 apps="git curl dialog" #add here your application
-ohmyzsh="https://raw.githubusercontent.com/Mr-Phil1/Linux/master/install-zsh.sh"
-zshconfig=""
+ohmyzsh="https://raw.githubusercontent.com/Mr-Phil1/Linux/master/install-zsh.sh" #path to my zsh install script
+zshconfig="" #path to my .zshrc config
+####################################################################################################################
 
-
+#this is the auto-install routine
 if [ $(dpkg-query -W -f='${Status}' $apps 2>/dev/null  | grep -c "ok installed") -eq 0 ];
 then
   echo "Install $apps"
   sudo apt install $apps -y;
-
 fi
 
-
+#dont touch this#
 cmd=(dialog --separate-output --checklist "Select options:" 22 75 13)
 options=(
     1 "System Update" on
