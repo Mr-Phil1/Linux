38bbf23f370dc39180918b74f489d131403cfdcf
Mr-Phil1 <36644192+Mr-Phil1@users.noreply.github.com>
2020-05-07 17:52:54 +0200

    add the install routine

--- menu.sh
+++ menu.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+if [ $(dpkg-query -W -f='${Status}' dialog 2>/dev/null  | grep -c "ok installed") -eq 0 ];
+then
+  echo "Install Dialog"
+  sudo apt install dialog -y;
+    if [ $(dpkg-query -W -f='${Status}' curl 2>/dev/null  | grep -c "ok installed") -eq 0 ];
+    then
+      echi "Install curl "
+      sudo sudo apt-get install curl -y;
+fi
