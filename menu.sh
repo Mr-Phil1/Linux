abf39b26e92fee959db008c5347de6808a5a9090
Mr-Phil1 <36644192+Mr-Phil1@users.noreply.github.com>
2020-06-12 18:41:24 +0200

    write a new menu script

--- menu.sh
+++ menu.sh
@@ -1,50 +1,84 @@
 #!/bin/bash
-######here  all the variables#######################################################################################
-apps="git curl dialog" #add here your application
-ohmyzsh="https://raw.githubusercontent.com/Mr-Phil1/Linux/master/install-zsh.sh" #path to my zsh install script
-zshconfig="" #path to my .zshrc config
-####################################################################################################################
+# utilitymenu.sh - A sample shell script to display menus on screen
+# Store menu options selected by the user
+INPUT=/tmp/menu.sh.$$
 
-#this is the auto-install routine
-if ! dpkg -s $apps >/dev/null 2>&1; then
-  sudo apt-get install $apps -y
-fi
+# Storage file for displaying cal and date command output
+OUTPUT=/tmp/output.sh.$$
 
-###dialog menu##
-cmd=(dialog --separate-output --checklist "Select options:" 22 75 13)
-options=(
-    1 "System Update" on
-    2 "Install Oh-my-Zsh" off
-    3 "Install Neofetch" off
-    4 "Remove ZSH + Oh-my-Zsh" off
-    5 "Make my .zshrc conf" off)
-choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-clear
-for choice in $choices
+# get text editor or fall back to vi_editor
+vi_editor=${EDITOR-nano /home/pi/.config/lxsession/LXDE-pi/autostart}
+
+# trap and delete temp files
+trap "rm $OUTPUT; rm $INPUT; exit" SIGHUP SIGINT SIGTERM
+
+#
+# Purpose - display output using msgbox
+#  $1 -> set msgbox height
+#  $2 -> set msgbox width
+#  $3 -> set msgbox title
+#
+function display_output(){
+	local h=${1-20}			# box height default 10
+	local w=${2-41} 		# box width default 41
+	local t=${3-Output} 	# box title
+	dialog --backtitle "Linux Shell Script Tutorial" --title "${t}" --clear --msgbox "$(<$OUTPUT)" ${h} ${w}
+}
+#
+# Purpose - display current system date & time
+#
+function show_date(){
+  clear
+	sudo reboot now
+}
+#
+# Purpose - display a calendar
+#
+function show_calendar(){
+	sudo mv /home/pi/.config/lxsession/LXDE-pi/autostart /home/pi/.config/lxsession/LXDE-pi/old/autostart_`date +"%d-%m-%Y_%H.%M"`.old
+  sudo rm /home/pi/.config/lxsession/LXDE-pi/autostart
+}
+
+function show_editor() {
+  sudo nano /home/pi/.config/lxsession/LXDE-pi/autostart
+}
+
+#function show_old() {
+#    ls /home/pi/.config/lxsession/LXDE-pi/old
+#}
+#
+# set infinite loop
+#
+while true
 do
-    case $choice in
-        1)
-          echo "sys update"
-      			sudo apt-get update -y && sudo apt-get upgrade -y
-            clear
-          ;;
-        2)
-          echo "Install Oh-my-ZSH"
-    		  	sh -c "$(curl -fsSL $ohmyzsh)"
-          ;;
-        3)
-          echo "Install Neofetch"
-    		  	sudo apt install neofetch -y
-          ;;
-        4)
-          echo "Remove Zsh + Oh-my-Zsh"
-            sudo apt purge zsh
-            sudo rm -r ${HOME}/.oh-my-zsh
-          ;;
-        5)
-          echo "Add my .zshrc conf"
-            sh -c "$(curl -fsSL $zshconfig)"
-          ;;
 
-    esac
+### display main menu ###
+dialog --clear  --help-button --backtitle "Linux Shell Script Tutorial" \
+--title "[ M A I N - M E N U ]" \
+--menu "You can use the UP/DOWN arrow keys, the first \n\
+letter of the choice as a hot key, or the \n\
+number keys 1-9 to choose an option.\n\
+Choose the TASK" 15 50 4 \
+Reboot "Reboot the system" \
+Remove "Remove autostart" \
+Editor "Edit the autostart" \
+#Old "View the old version"
+Exit "Exit to the shell" 2>"${INPUT}"
+
+menuitem=$(<"${INPUT}")
+
+
+# make decsion
+case $menuitem in
+	Reboot) show_date;;
+	Remove) show_calendar;;
+	Editor) show_editor;;
+#  Old) show_old;;
+	Exit) echo "Bye"; break;;
+esac
+
 done
+
+# if temp files found, delete em
+[ -f $OUTPUT ] && rm $OUTPUT
+[ -f $INPUT ] && rm $INPUT
