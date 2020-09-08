ad2cb9265e1f4f05c1cedc4d7cf7308dc916e602
Mr-Phil1 <mr.phil1@gmx.net>
2020-09-08 19:59:27 +0200

    change from menu to main-menu

--- zsh-conf.sh
+++ zsh-conf.sh
@@ -6,4 +6,4 @@ echo "#alias la=""ls -la""" >> ${HOME}/.zshrc
 echo "#alias cls="clear"" >> ${HOME}/.zshrc
 echo "#alias git-dir="cd ${HOME}/git"" >> ${HOME}/.zshrc
 echo "#alias rm-dir="sudo rm -r"" >> ${HOME}/.zshrc
-echo "alias menu="${HOME}/git/Linux/menu.sh"" >> ${HOME}/.zshrc
+echo "alias menu="${HOME}/git/Linux/main-menu.sh"" >> ${HOME}/.zshrc
