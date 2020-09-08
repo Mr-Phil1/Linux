7d068c5c60c860bb088cd6e35f671626dc2f5760
Mr-Phil1 <mr.phil1@gmx.net>
2020-09-08 19:10:27 +0200

    made some name name changes

---
+++ zsh-conf.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo "neofetch" >> ${HOME}/.zshrc
+echo ""			>> ${HOME}/.zshrc
+echo "#My Alias" >> ${HOME}/.zshrc
+echo "#alias la=""ls -la""" >> ${HOME}/.zshrc
+echo "#alias cls="clear"" >> ${HOME}/.zshrc
+echo "#alias git-dir="cd ${HOME}/git"" >> ${HOME}/.zshrc
+echo "#alias rm-dir="sudo rm -r"" >> ${HOME}/.zshrc
+echo "alias menu="${HOME}/git/Linux/menu.sh"" >> ${HOME}/.zshrc
