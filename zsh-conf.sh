41c349c523f09afa82c12247bed25f859cd8d0e1
Mr.  Phil <mr.phil1@gmx.net>
2021-09-24 19:55:34 +0200

    add rc-reload function in the zsh-conf.sh file

--- zsh-conf.sh
+++ zsh-conf.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
-echo "neofetch" >> ${HOME}/.zshrc
-echo ""			>> ${HOME}/.zshrc
-echo "#My Alias" >> ${HOME}/.zshrc
-echo "alias la=\"ls -la\"" >> ${HOME}/.zshrc
-echo "alias cls=\"clear\"" >> ${HOME}/.zshrc
-echo "alias git-dir=\"cd ${HOME}/git\"" >> ${HOME}/.zshrc
-echo "alias rm-dir=\"sudo rm -r\"" >> ${HOME}/.zshrc
-echo "alias menu=\"${HOME}/git/Linux/main-menu.sh\"" >> ${HOME}/.zshrc
+echo "neofetch" >>${HOME}/.zshrc
+echo "" >>${HOME}/.zshrc
+echo "#My Alias" >>${HOME}/.zshrc
+echo "alias la=\"ls -la\"" >>${HOME}/.zshrc
+echo "alias cls=\"clear\"" >>${HOME}/.zshrc
+echo "alias git-dir=\"cd ${HOME}/git\"" >>${HOME}/.zshrc
+echo "alias rm-dir=\"sudo rm -r\"" >>${HOME}/.zshrc
+echo "alias menu=\"${HOME}/git/Linux/main-menu.sh\"" >>${HOME}/.zshrc
+echo "alias rc-reload=\"clear && . ~/.zshrc\"" >>${HOME}/.zshrc
 
 clear && . ${HOME}/.zshrc
